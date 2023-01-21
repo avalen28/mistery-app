9e524f8880a987d239efb2c2ee46292e606d6ab7
Alberto <albertomenendez@MacBook-Pro-de-alberto.local>
2023-01-21 14:43:28 +0100

    post crreat

---
+++ routes/pet.js
@@ -0,0 +1,19 @@
+
+
+/* GET form view */
+/* ROUTE /shows/new */
+router.get('/new', function (req, res, next) {
+    res.render('newPet');
+  });
+  
+  /* POST get users show inputs */
+  /* ROUTE /shows/new */
+  router.post('/new', async function (req, res, next) {
+    const { name, gender, age, type, image, needsMedicine, description, owner } = req.body;
+    try {
+      const createdPet = await Pet.create({ name, gender, age, type, image, needsMedicine, description, owner });
+      res.redirect(`/pets/${createdPet._id}`);
+    } catch (error) {
+      next(error)
+    }
+  });
