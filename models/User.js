6bc801afa4fa386c37894f60d06d6e6f395f3da3
Alberto Valenzuela <avalen28@gmail.com>
2023-01-21 11:58:48 +0100

    fallo resuelto en modelo

--- models/User.js
+++ models/User.js
@@ -1,60 +1,62 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
-const userSchema = new Schema({
-  firstName: {
-    type: String,
-    trim: true,
-  },
-  lastName: {
-    type: String,
-    trim: true,
-  },
-  email: {
-    type: String,
-    required: [true, 'Please add a valid email'],
-    unique: true,
-    trim: true,
-    lowercase: true,
-    match: [/^\S+@\S+\.\S+$/, 'Please use a valid email address.'],
-  },
-  hashedPassword: {
-    type: String,
-    required: [true, 'Please add a password']
-  },
-  username: {
-    type: String,
-    required: [true, 'Please add a username'],
-    unique: true,
-    trim: true,
-  },
-  phone: {
-    type: Number,
-    unique: true,
-    trim: true,
-  },
-  city: {
-    type: String,
-    trim: true,
-  },
-  country: {
-    type: String,
-    trim: true,
-  },
-  role:{
-    type:String,
-    enum:{
-        values:['walker','owner'],
-        message:'That is not a valid role'
-    },
-    default: 'onwer'
-  },
-  required:true,
-},
+const userSchema = new Schema(
   {
-    timestamps: true
-  });
+    firstName: {
+      type: String,
+      trim: true,
+    },
+    lastName: {
+      type: String,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: [true, "Please add a valid email"],
+      unique: true,
+      trim: true,
+      lowercase: true,
+      match: [/^\S+@\S+\.\S+$/, "Please use a valid email address."],
+    },
+    hashedPassword: {
+      type: String,
+      required: [true, "Please add a password"],
+    },
+    username: {
+      type: String,
+      required: [true, "Please add a username"],
+      unique: true,
+      trim: true,
+    },
+    phone: {
+      type: Number,
+      unique: true,
+      trim: true,
+    },
+    city: {
+      type: String,
+      trim: true,
+    },
+    country: {
+      type: String,
+      trim: true,
+    },
+    role: {
+      type: String,
+      enum: {
+        values: ["walker", "owner"],
+        message: "That is not a valid role",
+      },
+      default: "onwer",
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema);
 
 module.exports = User;
