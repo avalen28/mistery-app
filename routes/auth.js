f5e0b4c376a5507682cd4030eb770b14e2c1c8f1
Alberto <albertomenendez@MacBook-Pro-de-alberto.local>
2023-01-21 11:21:34 +0100

    sign up POST

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require ("bcryptjs")
+const saltRounds = 6;
+
+const User = require("../models/User")
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
@@ -7,3 +11,29 @@ router.get('/', function (req, res, next) {
 });
 
 module.exports = router;
+
+// POST Sign up 
+
+router.post('/signup', async function (req, res, next) {
+  const { firstname, lastname, email, hashedPassword, username, phone, city, country, role,   } = req.body;
+  if (!username || !email || !password) {
+    res.render('auth/signup', { error: 'All fields are necessary.' });
+    return;
+  
+  }
+  try {
+    const userInDB = await User.findOne({ email: email });
+    if (userInDB) {
+      res.render('auth/signup', { error: `There already is a user with email ${email}` });
+      return;
+    } else {
+      const salt = await bcrypt.genSalt(saltRounds);
+      // const salt = bcrypt.genSaltSync(saltRounds);
+      const hashedPassword = await bcrypt.hash(password, salt);
+      const user = await User.create({ username, email, hashedPassword,role });
+      res.render('auth/profile', user);
+    }
+  } catch (error) {
+    next(error)
+  }
+});
