1e7b0baf595aed62117d221b52a00a508775e92c
Daniel-RG <21danielrondong@gmail.com>
2023-01-21 11:38:27 +0100

    ft post login

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../models/User');
+const bcrypt = require('bcrypt');
 
 /* GET logout */
 router.get('/logout', (req, res, next) => {
@@ -16,6 +18,34 @@ router.get('/logout', (req, res, next) => {
 /* GET Sign Up */
 router.get('/signup', function (req, res, next) {
   res.render('auth/signup');
+});
+
+/* POST log in view. */
+router.post('/login', async function (req, res, next) {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    res.render('auth/login', { error: 'Introduce email and password to log in' });
+    return;
+  }
+  try {
+    const userInDB = await User.findOne({ email: email });
+    if (!userInDB) {
+      res.render('auth/login', { error: `There are no users by ${email}` });
+      return;
+    } else {
+
+      const passwordMatch = await bcrypt.compare(password, userInDB.hashedPassword);
+      if (passwordMatch) {
+        req.session.currentUser = userInDB;
+        //res.render('auth/profile', userInDB);
+      } else {
+        res.render('auth/login', { error: 'Unable to authenticate user' });
+        return;
+      }
+    }
+  } catch (error) {
+    next(error)
+  }
 });
 
 module.exports = router;
